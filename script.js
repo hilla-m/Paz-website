dc90b08eeb02c490fd95cbde42197b085af87c23
hilla-m <hilla.manbar@gmail.com>
2021-03-03 11:59:14 +0200

    commit - homepage changing read more image

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-function chngImg1(){   
-    document.getElementById("img1").src = "images/read_more_blue.png";
+function imgBlue(imgId){   
+    document.getElementById(imgId).src = "images/read_more_blue.png";
+}
+
+function imgWhite(imgId){   
+    document.getElementById(imgId).src = "images/read_more_white.png";
+}
+
+function imgYellow(imgId){   
+    document.getElementById(imgId).src = "images/read_more_yellow.png";
 }
 
